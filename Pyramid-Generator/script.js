d746e340f70eb093fe90eb6072b1ae54bb7017f6
Butawantemi <japhetbuta@gmail.com>
2024-09-26 17:02:13 +0300

    JavaScript-freecodecamp

--- Pyramid-Generator/script.js
+++ Pyramid-Generator/script.js
@@ -1,12 +1,15 @@
+// Declare variables.
 const character = "!";
 const count = 10;
 const rows = [];
 let inverted = false;
 
+// Function to pad the row with spaces.
 function padRow(rowNumber, rowCount) {
     return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
+// Loop through the rows and add them to the array.
 for (let i = 1; i <= count; i++) {
     if (inverted) {
       rows.unshift(padRow(i, count));
@@ -17,6 +20,7 @@ for (let i = 1; i <= count; i++) {
 
 let result = ""
 
+// Loop through the rows and print them.
 for (const row of rows) {
   result = result + "\n" + row;
 }
