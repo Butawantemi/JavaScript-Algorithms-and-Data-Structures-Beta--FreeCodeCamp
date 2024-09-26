66de79260477a364914e6d8524da6d1e97f7d47e
Butawantemi <japhetbuta@gmail.com>
2024-09-26 16:56:33 +0300

    JavaScript-freecodecamp

--- Pyramid-Generator/script.js
+++ Pyramid-Generator/script.js
@@ -6,3 +6,11 @@ let inverted = false;
 function padRow(rowNumber, rowCount) {
     return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
+
+for (let i = 1; i <= count; i++) {
+    if (inverted) {
+      rows.unshift(padRow(i, count));
+    } else {
+      rows.push(padRow(i, count));
+    }
+  }
