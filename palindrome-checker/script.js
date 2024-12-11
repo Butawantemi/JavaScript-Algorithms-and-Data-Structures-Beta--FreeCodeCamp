e0f50f48bfca7ca83597a000a6cabad02708d0d6
Butawantemi <japhetbuta@gmail.com>
2024-12-11 16:31:02 +0300

    palindrome-checker

--- palindrome-checker/script.js
+++ palindrome-checker/script.js
@@ -5,3 +5,17 @@ const isPalindrome = str => {
 }
 
 const checkButton = document.getElementById('check-btn');
+
+const checkButton = document.getElementById('check-btn');
+checkButton.addEventListener('click', () => {
+    const inputElement = document.getElementById('text-input');
+    const resultElement = document.getElementById('result');
+    const inputValue = inputElement.value.trim();
+
+    if (!inputValue) {
+        alert('Please input a value');
+        return;
+    }
+
+    
+});
