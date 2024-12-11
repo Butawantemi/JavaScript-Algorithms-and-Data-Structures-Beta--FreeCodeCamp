51c481913ec1ebbaa94f35e1faaa753a6ea23db2
Butawantemi <japhetbuta@gmail.com>
2024-12-11 16:31:45 +0300

    palindrome-checker

--- palindrome-checker/script.js
+++ palindrome-checker/script.js
@@ -1,24 +1,24 @@
-const isPalindrome = str => {
-    const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
-    const reversedStr = cleanedStr.split('').reverse().join('');
-    return cleanedStr === reversedStr;
-}
+const isPalindrome = (str) => {
+  const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  const reversedStr = cleanedStr.split("").reverse().join("");
+  return cleanedStr === reversedStr;
+};
 
-const checkButton = document.getElementById('check-btn');
-checkButton.addEventListener('click', () => {
-    const inputElement = document.getElementById('text-input');
-    const resultElement = document.getElementById('result');
-    const inputValue = inputElement.value.trim();
+const checkButton = document.getElementById("check-btn");
+checkButton.addEventListener("click", () => {
+  const inputElement = document.getElementById("text-input");
+  const resultElement = document.getElementById("result");
+  const inputValue = inputElement.value.trim();
 
-    if (!inputValue) {
-        alert('Please input a value');
-        return;
-    }
+  if (!inputValue) {
+    alert("Please input a value");
+    return;
+  }
 
-    const isInputPalindrome = isPalindrome(inputValue);
-    if (isInputPalindrome) {
-        resultElement.textContent = `${inputValue} is a palindrome`;
-    } else {
-        resultElement.textContent = `${inputValue} is not a palindrome`;
-    }
+  const isInputPalindrome = isPalindrome(inputValue);
+  if (isInputPalindrome) {
+    resultElement.textContent = `${inputValue} is a palindrome`;
+  } else {
+    resultElement.textContent = `${inputValue} is not a palindrome`;
+  }
 });
