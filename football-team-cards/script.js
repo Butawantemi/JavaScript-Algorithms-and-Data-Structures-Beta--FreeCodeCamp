32729f43173645797f2e84a56261f8d13caf2fb7
Butawantemi <japhetbuta@gmail.com>
2024-12-13 20:02:05 +0300

    football-team-cards

--- football-team-cards/script.js
+++ football-team-cards/script.js
@@ -4,3 +4,30 @@ const worldCupYear = document.getElementById("year");
 const headCoach = document.getElementById("head-coach");
 const playerCards = document.getElementById("player-cards");
 const playersDropdownList = document.getElementById("players");
+const myFavoriteFootballTeam = {
+    team: "Argentina",
+    sport: "Football",
+    year: 1986,
+    isWorldCupWinner: true,
+    headCoach: {
+      coachName: "Carlos Bilardo",
+      matches: 7,
+    },
+    players: [
+      {
+        name: "Sergio Almirón",
+        position: "forward",
+        number: 1,
+        isCaptain: false,
+        nickname: null,
+      },
+      {
+        name: "Sergio Batista",
+        position: "midfielder",
+        number: 2,
+        isCaptain: false,
+        nickname: null,
+      },
+       
+    ],
+  };
