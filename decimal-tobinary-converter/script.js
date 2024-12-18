88ac510846bc2a84c2336b1c33ecc448fa27e04a
Butawantemi <japhetbuta@gmail.com>
2024-12-18 12:20:53 +0300

    decimal to binary converter

--- decimal-tobinary-converter/script.js
+++ decimal-tobinary-converter/script.js
@@ -62,18 +62,18 @@ const showAnimation = () => {
 };
 
 const checkUserInput = () => {
-    const inputInt = parseInt(numberInput.value);
-  
-    if (!numberInput.value || isNaN(inputInt) || inputInt < 0) {
-      alert("Please provide a decimal number greater than or equal to 0");
-      return;
-    }
-  
-    if (inputInt === 5) {
-      showAnimation();
-      return;
-    }
-  
-    result.textContent = decimalToBinary(inputInt);
-    numberInput.value = "";
-  };
+  const inputInt = parseInt(numberInput.value);
+
+  if (!numberInput.value || isNaN(inputInt) || inputInt < 0) {
+    alert("Please provide a decimal number greater than or equal to 0");
+    return;
+  }
+
+  if (inputInt === 5) {
+    showAnimation();
+    return;
+  }
+
+  result.textContent = decimalToBinary(inputInt);
+  numberInput.value = "";
+};
